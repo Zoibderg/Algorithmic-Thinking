ca4cb3913b5cd327e8fa4c22f4525fce62385f71
Zoibderg <43228316+Zoibderg@users.noreply.github.com>
2022-04-10 00:08:23 -0700

    snowflake.py comment and clean

--- HASHTABLES/UniqueSnowflake/snowflake.py
+++ HASHTABLES/UniqueSnowflake/snowflake.py
@@ -8,16 +8,19 @@ class Snowflake:
         self.n = n
 
     def identify_identical(self) -> str:
+        # set pointers
         i = 0
-        j = i + 1
-        found, notfound = "Twin integers found.\n", "No two integers are alike.\n"
+        j = i + 1  # we never want j == i
+        # start search
         while i < self.n:
             while j < self.n:
-                if self.values[i] == self.values[j]:
-                    return found
+                if self.values[i] == self.values[j]:  # integers are the same
+                    return "Twin integers found.\n"
+            # increment pointers
                 j += 1
             i += 1
-        return notfound
+        # no like integers found
+        return "No two integers are alike.\n"
 
 if __name__ == '__main__':
     a = [1, 2, 3, 1, 5]
