a7ef18020d8e48299ffa36b58932086ccc701f05
Zoibderg <43228316+Zoibderg@users.noreply.github.com>
2022-04-12 12:39:55 -0700

    minor formatting in tests

--- HASHTABLES/UniqueSnowflake/tests/test.py
+++ HASHTABLES/UniqueSnowflake/tests/test.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 import sys,os
 BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, BASE)
@@ -17,29 +18,34 @@ class TestSnowflake(unittest.TestCase):
     def test_identify_identicalTrue(self):  # sourcery skip: class-extract-method
         sf = Snowflake([1, 2, 3, 1, 5])
         ii = sf.identify_identical()
+
         self.assertEqual(ii, "Twin integers found.\n")
 
     def test_identify_identicalFalse(self):
         sf = Snowflake([1, 2, 3, 4, 5])
         ii = sf.identify_identical()
+
         self.assertEqual(ii, "No two integers are alike.\n")
 
     def test_identify_rightTrue(self):
         sf1 = self.sf([1, 2, 3, 4, 5, 6])
         sf2 = self.sf([4, 5, 6, 1, 2, 3])
         ir = self.us.identical_right(sf1, sf2)
+
         self.assertEqual(ir, True)
 
     def test_identify_rightFalse(self):
         sf1 = self.sf([1, 2, 3, 12, 5, 6])
         sf2 = self.sf([4, 5, 6, 1, 2, 3])
         ir = self.us.identical_right(sf1, sf2)
+
         self.assertEqual(ir, False)
 
     def test_identify_leftTrue(self):
         sf1 = self.sf([1, 2, 3, 4, 5, 6])
         sf2 = self.sf([4, 5, 6, 1, 2, 3])
         il = self.us.identical_left(sf1, sf2)
+
         self.assertEqual(il, True)
 
     def test_identify_leftFalse(self):
@@ -52,12 +58,14 @@ class TestSnowflake(unittest.TestCase):
         sf1 = self.sf([1, 2, 3, 4, 5, 6])
         sf2 = self.sf([4, 5, 6, 1, 2, 3])
         ai = self.us.are_identical(sf1, sf2)
+
         self.assertEqual(ai, True)
 
     def test_are_identicalFalse(self):
         sf1 = self.sf([1, 2, 3, 12, 5, 6])
         sf2 = self.sf([4, 5, 6, 1, 2, 3])
         ai = self.us.are_identical(sf1, sf2)
+
         self.assertEqual(ai, False)
 
 if __name__ == '__main__':
