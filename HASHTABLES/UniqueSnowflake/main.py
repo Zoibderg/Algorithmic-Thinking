c6d6826f9f2ad31f34104e7fc6c5250268110fbb
Zoibderg <43228316+Zoibderg@users.noreply.github.com>
2022-04-21 13:43:49 -0700

    added input.txt for reading in large amounts of snowflakes/finished read_file in main

--- HASHTABLES/UniqueSnowflake/main.py
+++ HASHTABLES/UniqueSnowflake/main.py
@@ -8,16 +8,27 @@ class ReadSnowflakes:
 
     def __init__(self):
         self.snowflakes = []
-        self.n
-        self.coutner = 0
+        self.n = 0
 
-        with open('self', 'r') as f:
+    def read_file(self, file):
+        counter = 0
+        with open(file, 'r') as f:
             for line in f:
-                if self.coutner > 0:
+                if counter <= 0:
                     self.n = int(line)
                 else:
-                    for integer in line:
-                        snowflake = [integer]
+                    snowflake = [int(integer) for integer in line if integer != '\n']
+                    snowflake = Snowflake(snowflake)
                     self.snowflakes.append(snowflake)
                 counter += 1
-                
+
+    def findUsnowflakes(self):
+        for i, j in range(len(self.snowflakes)):
+            UniqueSnowflake.identify_identical_snowflakes(self.snowflakes[i], 
+            self.snowflakes[j], self.n)
+        
+
+if __name__ == '__main__':
+    RFlake = ReadSnowflakes()
+    RFlake.read_file('/Users/austintesch/Documents/GitHub/Algorithmic_Thinking/HASHTABLES/UniqueSnowflake/input.txt')
+    RFlake.findUsnowflakes()
