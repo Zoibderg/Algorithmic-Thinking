328cf1ec115d67a06f862f13b220b7b734fca8a3
Zoibderg <43228316+Zoibderg@users.noreply.github.com>
2022-04-30 12:58:02 -0700

    comments

--- HASHTABLES/UniqueSnowflake/main.py
+++ HASHTABLES/UniqueSnowflake/main.py
@@ -17,20 +17,27 @@ class ReadSnowflakes:
         self.n = 0  # total amount of snoflakes stored
 
     def read_file(self, file):
+        """
+        read thorough file and build snowflakes
+        """
         with open(file, 'r') as f:
+            # first line contains total number of snowflakes
             head = next(f)
             self.n = int(head)
-            snowflakes = [line.rstrip('\n') for line in f]
-            snowflakes = [[int(i) for i in s.split(' ')] for s in snowflakes]
-            snowflakes = [Snowflake(s) for s in snowflakes]
+            # build snowflakes
+            snowflakes = [line.rstrip('\n') for line in f]  # strip new line from lines
+            snowflakes = [[int(i) for i in s.split(' ')] for s in snowflakes]  # build snowflakes from integers
+            snowflakes = [Snowflake(s) for s in snowflakes]  # set snowflakes as snowflakes
             self.snowflakes = snowflakes
 
     def findUsnowflakes(self):
+        # call to identify unique tiwn snowflakes
         return UniqueSnowflake.identify_unique_identical_snowflakes(self.snowflakes, self.n)
         
 
 if __name__ == '__main__':
     RFlake = ReadSnowflakes()
-    RFlake.read_file('./HASHTABLES/UniqueSnowflake/input.txt')
+    RFlake.read_file('./HASHTABLES/UniqueSnowflake/input.txt')  # read input
+    # show prompt of engagement
     print(f"Checking {RFlake.n} snowflakes for unique identical twins ...\n")
-    print(RFlake.findUsnowflakes())
+    print(RFlake.findUsnowflakes())  # return result
